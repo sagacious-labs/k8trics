49eec0402705ec1dd42ca5b63076c5c40926b6ad
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-12 16:45:03 +0530

    add basic utility functions
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ pkg/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import "os"
+
+// GetEnv takes in the environmental variable key and a fallback
+// if the env var is absent then the fallback is returned or else the
+// variable will be returned
+func GetEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return fallback
+	}
+
+	return fallback
+}
