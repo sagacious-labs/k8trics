75e13e7339fb14dce27e2ed67861b4f128de50a9
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-20 19:37:42 +0530

    add logging defaults
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "os"
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 // GetEnv takes in the environmental variable key and a fallback
 // if the env var is absent then the fallback is returned or else the
@@ -11,4 +15,29 @@ func GetEnv(key, fallback string) string {
 	}
 
 	return fallback
+}
+
+func SetupLogger() {
+	logrus.SetReportCaller(true)
+
+	logLevel := logrus.WarnLevel
+
+	switch GetEnv("K8TRICS_LOG_LEVEL", "warn") {
+	case "panic":
+		logLevel = logrus.PanicLevel
+	case "fatal":
+		logLevel = logrus.FatalLevel
+	case "error":
+		logLevel = logrus.ErrorLevel
+	case "warn":
+		logLevel = logrus.WarnLevel
+	case "info":
+		logLevel = logrus.InfoLevel
+	case "debug":
+		logLevel = logrus.DebugLevel
+	case "trace":
+		logLevel = logrus.TraceLevel
+	}
+
+	logrus.SetLevel(logLevel)
 }
