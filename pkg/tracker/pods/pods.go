99ccdde17944bd9120fb1d7f59b4c17e1dab8fcd
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2021-11-20 15:30:41 +0530

    add resource trackers
    Signed-off-by: Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>

---
+++ pkg/tracker/pods/pods.go
@@ -0,0 +1,46 @@
+package pods
+
+import (
+	"github.com/sagacious-labs/k8trics/pkg/store"
+	corev1 "k8s.io/api/core/v1"
+	coreinformer "k8s.io/client-go/informers/core/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+// Tracker is a struct representing a Pod tracker
+type Tracker struct {
+	store    *store.PodStore
+	informer coreinformer.PodInformer
+}
+
+// New returns pointer to a Pod Tracker
+func New(informer coreinformer.PodInformer, store *store.PodStore) *Tracker {
+	return &Tracker{
+		store:    store,
+		informer: informer,
+	}
+}
+
+// Start attahes the tracker handlers
+func (t *Tracker) Start() {
+	t.informer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    t.handleAdd,
+		DeleteFunc: t.handleDelete,
+	})
+}
+
+func (t *Tracker) handleAdd(obj interface{}) {
+	casted, ok := obj.(corev1.Pod)
+	if ok {
+		println("success create")
+		t.store.Upsert(casted)
+	}
+}
+
+func (t *Tracker) handleDelete(obj interface{}) {
+	casted, ok := obj.(corev1.Pod)
+	if ok {
+		println("success delete")
+		t.store.Delete(casted.GetName(), casted.GetNamespace())
+	}
+}
---
+++ pkg/tracker/tracker.go
@@ -0,0 +1,35 @@
+package tracker
+
+import (
+	"github.com/sagacious-labs/k8trics/pkg/k8s"
+	"github.com/sagacious-labs/k8trics/pkg/store"
+	"github.com/sagacious-labs/k8trics/pkg/tracker/pods"
+)
+
+// Tracker
+type Tracker struct {
+	pod      *pods.Tracker
+	khandler *k8s.K8s
+
+	stop chan struct{}
+}
+
+func New(khandler *k8s.K8s, store *store.PodStore) *Tracker {
+	pods.New(khandler.Informers().Core().V1().Pods(), store)
+
+	return &Tracker{
+		khandler: khandler,
+		pod:      pods.New(khandler.Informers().Core().V1().Pods(), store),
+		stop:     make(chan struct{}),
+	}
+}
+
+func (t *Tracker) Start() {
+	t.khandler.Informers().Start(t.stop)
+
+	t.pod.Start()
+}
+
+func (t *Tracker) Stop() {
+	t.stop <- struct{}{}
+}
